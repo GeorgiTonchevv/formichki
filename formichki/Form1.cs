5fcc794453d9cb9fe84dec8491234ae20673e7a4
george tonchev <joro_manqka1_2000@abv.bg>
2023-01-18 17:55:27 +0200

    Made Rectangles w RGB
    mn cool

--- formichki/Form1.cs
+++ formichki/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+
 
 namespace formichki
 {
     public partial class Form1 : Form
     {
+        private static readonly Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        Thread th;
+        Thread th1;
+        Random rdm;
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            th = new Thread(threadsquare);
+            th.Start();
+        }
+        public void threadsquare()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(RandomColor(), 4), new Rectangle(rdm.Next(0, this.Width),rdm.Next(0, this.Height), 40, 20));
+                Thread.Sleep(100);
+            }
+            MessageBox.Show("Completed");
+        }
+        public void threadtriangle()
+        {
+            //th1 = new Thread(threadtriangle);
+            //th1.Start();
+        }
+        private Color RandomColor()
+        {
+            return Color.FromArgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdm = new Random();
         }
     }
 }
